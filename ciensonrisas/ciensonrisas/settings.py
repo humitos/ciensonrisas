13018cafed15b7637894ed12a2a93eb7c124fdf2
Manuel Kaufmann <humitos@gmail.com>
2012-07-17 15:14:17 -0300

    Search static content one up directory

--- ciensonrisas/ciensonrisas/settings.py
+++ ciensonrisas/ciensonrisas/settings.py
@@ -52,7 +52,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'user-media')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, '..', 'user-media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -63,7 +63,7 @@ MEDIA_URL = '/user-media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '..', 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -74,7 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'site-media'),
+    os.path.join(PROJECT_ROOT, '..', 'site-media'),
 )
 
 # List of finder classes that know how to find static files in
@@ -114,7 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'templates',
+    os.path.join(PROJECT_ROOT, '..', 'templates'),
 )
 
 INSTALLED_APPS = (
