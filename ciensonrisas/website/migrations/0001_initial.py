7b8bcd781dc1fff783c37d0d88c1a4582ef8b42e
Manuel Kaufmann <humitos@gmail.com>
2012-07-17 16:25:21 -0300

    Models organized
    Place, author and smile working

--- ciensonrisas/website/migrations/0001_initial.py
+++ ciensonrisas/website/migrations/0001_initial.py
@@ -24,8 +24,21 @@ class Migration(SchemaMigration):
             ('description', self.gf('django.db.models.fields.TextField')()),
             ('photo', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
             ('place', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['website.Place'])),
+            ('author', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['website.Author'])),
         ))
         db.send_create_signal('website', ['Smile'])
+
+        # Adding model 'Author'
+        db.create_table('website_author', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=30)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=30)),
+            ('nickname', self.gf('django.db.models.fields.CharField')(max_length=30)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('photo', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('sign', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+        ))
+        db.send_create_signal('website', ['Author'])
 
 
     def backwards(self, orm):
@@ -35,8 +48,21 @@ class Migration(SchemaMigration):
         # Deleting model 'Smile'
         db.delete_table('website_smile')
 
+        # Deleting model 'Author'
+        db.delete_table('website_author')
+
 
     models = {
+        'website.author': {
+            'Meta': {'object_name': 'Author'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'nickname': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'sign': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'})
+        },
         'website.place': {
             'Meta': {'object_name': 'Place'},
             'description': ('django.db.models.fields.TextField', [], {}),
@@ -46,6 +72,7 @@ class Migration(SchemaMigration):
         },
         'website.smile': {
             'Meta': {'object_name': 'Smile'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['website.Author']"}),
             'description': ('django.db.models.fields.TextField', [], {}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'number': ('django.db.models.fields.IntegerField', [], {}),
--- ciensonrisas/website/models.py
+++ ciensonrisas/website/models.py
@@ -14,4 +14,20 @@ class Smile(models.Model):
     number = models.IntegerField()
     description = models.TextField()
     photo = models.ImageField(upload_to='smiles')
-    place = models.ForeignKey(Place)
+    place = models.ForeignKey('Place')
+    author = models.ForeignKey('Author')
+
+    def __unicode__(self):
+        return '# %s' % self.number
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=30)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='authors')
+    sign = models.ImageField(upload_to='signs')
+
+    def __unicode__(self):
+        return self.nickname
