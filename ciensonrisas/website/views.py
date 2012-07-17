1ee1f62a5f892d66446702d20f80c24b7d4ea96c
Manuel Kaufmann <humitos@gmail.com>
2012-07-17 17:07:43 -0300

    Section "miralas" example
    Some photos there to have a clue about it

--- ciensonrisas/website/views.py
+++ ciensonrisas/website/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.views.generic import DetailView
+from website.models import Place
+
+
+class PlaceDetailView(DetailView):
+
+    queryset = Place.objects.all()
+
+
+    def get_object(self):
+        # Call the superclass
+        object = super(PlaceDetailView, self).get_object()
+        # Record the last accessed date
+        # object.last_accessed = datetime.datetime.now()
+        # object.save()
+        # Return the object
+        return object
